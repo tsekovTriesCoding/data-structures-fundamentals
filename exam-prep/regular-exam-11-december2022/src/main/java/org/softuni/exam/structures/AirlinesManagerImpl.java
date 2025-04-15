c8b5b7ccf9f0b105856aeae521bd664c40b7578f
Tsvetoslav Tsekov <129774811+tsekovTriesCoding@users.noreply.github.com>
2025-04-15 16:05:21 +0300

    Airlines

--- exam-prep/regular-exam-11-december2022/src/main/java/org/softuni/exam/structures/AirlinesManagerImpl.java
+++ exam-prep/regular-exam-11-december2022/src/main/java/org/softuni/exam/structures/AirlinesManagerImpl.java
@@ -1,61 +1,109 @@
 package org.softuni.exam.structures;
 
 import org.softuni.exam.entities.Airline;
+import org.softuni.exam.entities.Deliverer;
 import org.softuni.exam.entities.Flight;
 
+import java.sql.Struct;
+import java.util.*;
+import java.util.stream.Collectors;
+
 public class AirlinesManagerImpl implements AirlinesManager {
+    private Map<String, Airline> airlines = new HashMap<>();
+    private Map<String, Flight> flights = new HashMap<>();
+    private Map<String, Set<Flight>> flightsByAirline = new HashMap<>();
+    private Set<Flight> completedFlights = new HashSet<>();
+
     @Override
     public void addAirline(Airline airline) {
-
+        this.airlines.put(airline.getId(), airline);
+        this.flightsByAirline.put(airline.getId(), new HashSet<>());
     }
 
     @Override
     public void addFlight(Airline airline, Flight flight) {
+        if (!this.airlines.containsKey(airline.getId())) {
+            throw new IllegalArgumentException();
+        }
 
+        this.flights.put(flight.getId(), flight);
+        this.flightsByAirline.get(airline.getId()).add(flight);
+
+        if (flight.isCompleted()) {
+            this.completedFlights.add(flight);
+        }
     }
 
     @Override
     public boolean contains(Airline airline) {
-        return false;
+        return this.airlines.containsKey(airline.getId());
     }
 
     @Override
     public boolean contains(Flight flight) {
-        return false;
+        return this.flights.containsKey(flight.getId());
     }
 
     @Override
     public void deleteAirline(Airline airline) throws IllegalArgumentException {
+        if (!this.airlines.containsKey(airline.getId())) {
+            throw new IllegalArgumentException();
+        }
 
+        this.airlines.remove(airline.getId());
+        Set<Flight> removedFlights = this.flightsByAirline.remove(airline.getId());
+
+        removedFlights.forEach(flight -> this.flights.remove(flight.getId()));
     }
 
     @Override
     public Iterable<Flight> getAllFlights() {
-        return null;
+        return this.flights.values();
     }
 
     @Override
     public Flight performFlight(Airline airline, Flight flight) throws IllegalArgumentException {
-        return null;
+        if (!this.airlines.containsKey(airline.getId()) || !this.flights.containsKey(flight.getId())) {
+            throw new IllegalArgumentException();
+        }
+
+        this.flights.get(flight.getId()).setCompleted(true);
+        this.completedFlights.add(flight);
+
+        return flight;
     }
 
     @Override
     public Iterable<Flight> getCompletedFlights() {
-        return null;
+        return this.completedFlights;
     }
 
     @Override
     public Iterable<Flight> getFlightsOrderedByNumberThenByCompletion() {
-        return null;
+        return this.flights.values()
+                .stream()
+                .sorted(Comparator.comparing(Flight::isCompleted)
+                        .thenComparing(Flight::getNumber))
+                .collect(Collectors.toList());
     }
 
     @Override
     public Iterable<Airline> getAirlinesOrderedByRatingThenByCountOfFlightsThenByName() {
-        return null;
+        return this.airlines.values()
+                .stream()
+                .sorted(Comparator.comparing(Airline::getRating)
+                        .thenComparingInt(f -> this.flightsByAirline.get(f.getId()).size()).reversed()
+                        .thenComparing(Airline::getName))
+                .collect(Collectors.toList());
     }
 
     @Override
     public Iterable<Airline> getAirlinesWithFlightsFromOriginToDestination(String origin, String destination) {
-        return null;
+        return this.airlines.values()
+                .stream()
+                .filter(a -> this.flightsByAirline.get(a.getId()).stream()
+                        .anyMatch(flight -> !flight.isCompleted() && flight.getOrigin().equals(origin)
+                                && flight.getDestination().equals(destination)))
+                .collect(Collectors.toList());
     }
 }
